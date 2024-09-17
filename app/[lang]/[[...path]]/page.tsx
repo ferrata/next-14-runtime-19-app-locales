e284012857d7add396c997f3f2d16f876f0ef23e
Sasha Goloshchapov <ferrata@hey.com>
2024-09-17 17:37:40 -0500

    chore: add localization

--- app/[lang]/[[...path]]/page.tsx
+++ app/[lang]/[[...path]]/page.tsx
@@ -4,22 +4,37 @@ import { Page as MakeswiftPage } from '@makeswift/runtime/next'
 import { getSiteVersion } from '@makeswift/runtime/next/server'
 
 import { client } from '@/lib/makeswift/client'
+import '@/lib/makeswift/components'
 
-type ParsedUrlQuery = { path?: string[] }
+type ParsedUrlQuery = {
+  lang: string
+  path?: string[]
+}
 
 export async function generateStaticParams() {
-  return await client
-    .getPages()
-    .map(page => ({
-      path: page.path.split('/').filter(segment => segment !== ''),
-    }))
-    .toArray()
+  const pages = await client.getPages().toArray()
+
+  return pages.flatMap(page => [
+    {
+      params: {
+        path: page.path.split('/').filter(segment => segment !== ''),
+        lang: page.locale,
+      },
+    },
+    ...page.localizedVariants.map(variant => ({
+      params: {
+        path: variant.path.split('/').filter(segment => segment !== ''),
+        lang: variant.locale,
+      },
+    })),
+  ])
 }
 
 export default async function Page({ params }: { params: ParsedUrlQuery }) {
   const path = '/' + (params?.path ?? []).join('/')
   const snapshot = await client.getPageSnapshot(path, {
     siteVersion: getSiteVersion(),
+    locale: params.lang,
   })
 
   if (snapshot == null) return notFound()
